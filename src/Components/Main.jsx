d3311ec294132bf602fdc2e314237cb2d77272dc
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-07-21 23:26:22 +0630

    Finished testimonals component design for desktop

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -62,19 +62,16 @@ export default function Main(){
         </Cards>
         </section>
 
-        
+        <section className="testimonials">
+        <Testimonials name="May Andersons" job="Workcation, CTO" 
+        image ={userImage}
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit. </Testimonials>
+        </section>
 
         <section className="testimonials">
         <Testimonials name="May Andersons" job="Workcation, CTO" 
         >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit. </Testimonials>
         </section>
-
-         <section className="testimonials">
-        <Testimonials name="May Andersons" job="Workcation, CTO" 
-        image ={userImage}
-        >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit. </Testimonials>
-        </section>
-        
-        </main>
+    </main>
     )
 }
